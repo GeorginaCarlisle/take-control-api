54571e94a23bf141bbb3a8b82d50d693e1e15857
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-03 21:35:23 +0000

    FEAT: Install all dependencies, connect cloudinary and database and prep for deployment

---
+++ take_control_api/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for take_control_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'take_control_api.settings')
+
+application = get_wsgi_application()
