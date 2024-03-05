3935535cfa5cd4a35d413ef9a3acd8662927b7a3
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-05 16:18:33 +0000

    FEAT: Add tests for Focus detail view

--- TESTING.md
+++ TESTING.md
@@ -32,4 +32,17 @@ Automated tests have been created for all views
 | test_focus_create_no_name_throws_error | Logged in user sending post request without name data, should return 400 error | Pass |
 | test_focus_view_own_focus_only | Logged in user sending get request, receives only their focuses | Pass |
 
+### FocusDetailView
+
+| Test name | Description | Outcome |
+| --- | ---- | -- |
+| test_logged_in_can_get_their_focus_detail | Logged in user sending a get request for a focus they own, should return focus | Pass |
+| test_logged_out_no_access_focus_detail | Logged out user sending a get request for a focus, should return access denied | Pass |
+| test_invalid_focus_request_handled | Logged in user sending a get request for a focus that doesn't exist, should return 404 not found | Pass |
+| test_logged_in_denied_get_focus_dont_own | Logged in user sending get request for focus they don't own, should return access denied | Pass |
+| test_logged_in_owner_can_edit_their_focus | Logged in user sending a put request for owned focus, should return ok and make changes | Pass |
+| test_logged_in_owner_denied_edit_focus_dont_own | Logged in user sending a put request for focus they dont own, should return access denied | Pass |
+| test_logged_in_owner_can_delete_their_focus | Logged in user sending a delete request for owned focus, should return ok and delete focus | Pass |
+| test_logged_in_owner_denied_delete_focus_dont_own | Logged in user sending a delete request for focus they don't own, should return access denied | Pass |
+
 [Return to contents list](#contents)
--- focus/tests.py
+++ focus/tests.py
@@ -65,3 +65,86 @@ class FocusListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
 
+class FocusDetailViewTests(APITestCase):
+    """
+    Tests for the Focus Detail view
+    """
+    def setUp(self):
+        FirstTester = User.objects.create_user(username='FirstTester', password='pass')
+        Focus.objects.create(
+            owner=FirstTester, name="Name", why="Why"
+        )
+        SecondTester = User.objects.create_user(username='SecondTester', password='word')
+        Focus.objects.create(
+            owner=SecondTester, name="Test", why="To test"
+        )
+    
+    def test_logged_in_can_get_their_focus_detail(self):
+        """
+        Logged in user sending a get request for a focus they own, should return focus
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.get('/focus/1')
+        focus = response.data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(focus['owner'], 'FirstTester')
+        self.assertEqual(focus['name'], 'Name')
+
+    def test_logged_out_no_access_focus_detail(self):
+        """
+        Logged out user sending a get request for a focus, should return access denied
+        """
+        response = self.client.get('/focus/1')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_invalid_focus_request_handled(self):
+        """
+        Logged in user sending a get request for a focus that doesn't exist, should return 404 not found
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.get('/focus/3')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_logged_in_denied_get_focus_dont_own(self):
+        """
+        Logged in user sending get request for focus they don't own, should return access denied
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.get('/focus/2')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_logged_in_owner_can_edit_their_focus(self):
+        """
+        Logged in user sending a put request for owned focus, should return ok and make changes
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.put('/focus/1', {'name': 'name changed'})
+        focus = response.data
+        self.assertEqual(focus['name'], 'name changed')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_owner_denied_edit_focus_dont_own(self):
+        """
+        Logged in user sending a put request for focus they dont own, should return access denied
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.put('/focus/2', {'name': 'name changed'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)  
+
+    def test_logged_in_owner_can_delete_their_focus(self):
+        """
+        Logged in user sending a delete request for owned focus, should return ok and delete focus
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.delete('/focus/1')
+        count = Focus.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_logged_in_owner_denied_delete_focus_dont_own(self):
+        """
+        Logged in user sending a delete request for focus they don't own, should return access denied
+        """
+        self.client.login(username='FirstTester', password='pass')
+        response = self.client.delete('/focus/2')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
