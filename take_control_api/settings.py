bc0f9a54c2d51d2e7d34345d0fe1a081ef88c9de
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-05 16:45:21 +0000

    FIX: Remove default permissions

--- take_control_api/settings.py
+++ take_control_api/settings.py
@@ -33,9 +33,6 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
