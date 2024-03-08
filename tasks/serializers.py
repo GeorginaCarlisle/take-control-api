e7dfd4612ab66f59c07091d86c92c04f6b1635fb
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-08 11:03:55 +0000

    FEAT: Add extra fields to Task serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -14,16 +14,18 @@ class TaskSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    deadline_near = serializers.SerializerMethodField()
-    goal_deadline_near = serializers.SerializerMethodField()
+    deadline_info = serializers.SerializerMethodField()
+    goal_deadline = serializers.SerializerMethodField()
+    goal_deadline_info = serializers.SerializerMethodField()
     goal_name = serializers.SerializerMethodField()
     active = serializers.SerializerMethodField()
+    focus_rank = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
-    def get_deadline_near(self, obj):
+    def get_deadline_info(self, obj):
         """
         Generates a new field containing information if the deadline is less than 2 days away
         """
@@ -37,29 +39,35 @@ class TaskSerializer(serializers.ModelSerializer):
             elif days_remaining == 1:
                 return "Due Tomorrow!!"
             else:
-                return None
+                easy_date = future_deadline.strftime('%d/%m/%y')
+                return f'Due {easy_date}'
         else:
             return None
 
-    def get_goal_deadline_near(self, obj):
+    def get_goal_deadline(self, obj):
+        """
+        Generates a new field containing the linked goals deadline,
+        if there is a linked goal
+        """
+        if obj.goal:
+            return obj.goal.deadline
+        else:
+            return None
+
+    def get_goal_deadline_info(self, obj):
         """
         Generates a new field containing information if the linked goal is near
         """
-        if obj.goal:
-            goal_id = obj.goal.id
-            goal = Goal.objects.get(id=goal_id)
-            goal_deadline = goal.deadline
-            if goal_deadline:
-                today_naive = datetime.now()
-                today_aware = today_naive.replace(tzinfo=timezone.utc)
-                days_remaining = (goal_deadline - today_aware).days
-                easy_date = goal_deadline.strftime('%d/%m/%y')
-                if days_remaining<=7:
-                    return f'Goal due on the {easy_date} only {days_remaining} days to go'
-                else:
-                    return f'Goal due on the {easy_date}'
+        goal_deadline = self.get_goal_deadline(obj)
+        if goal_deadline:
+            today_naive = datetime.now()
+            today_aware = today_naive.replace(tzinfo=timezone.utc)
+            days_remaining = (goal_deadline - today_aware).days
+            easy_date = goal_deadline.strftime('%d/%m/%y')
+            if days_remaining<=7:
+                return f'Goal due on the {easy_date} only {days_remaining} days to go'
             else:
-                return None
+                return f'Goal due on the {easy_date}'
         else:
             return None
 
@@ -79,7 +87,7 @@ class TaskSerializer(serializers.ModelSerializer):
             else:
                 return "Miscellaneous"
 
-    def get_active(self,obj):
+    def get_active(self, obj):
         """
         generates a new field that is true if no goal, or goal is active
         and false if goal is not active
@@ -93,6 +101,17 @@ class TaskSerializer(serializers.ModelSerializer):
                 return False
         else:
             return True
+
+    def get_focus_rank(self, obj):
+        """
+        Generates a new field that inherits from the focus_rank field of
+        the connected focus. If no connected focus field is given the value None
+        """
+        if obj.focus:
+            return obj.focus.rank
+        else:
+            return None
+
 
     class Meta:
         model = Task
@@ -109,8 +128,10 @@ class TaskSerializer(serializers.ModelSerializer):
             'name',
             'deadline',
             'labels',
-            'deadline_near',
-            'goal_deadline_near',
+            'deadline_info',
+            'goal_deadline',
+            'goal_deadline_info',            
             'goal_name',
-            'active'
+            'active',
+            'focus_rank'
         ]
