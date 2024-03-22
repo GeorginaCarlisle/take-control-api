3f5152fbc58bccb70cdb0e7e4b39146bf0f16e73
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-03-22 16:32:03 +0000

    FEAT: Increase page_size to 30

--- take_control_api/settings.py
+++ take_control_api/settings.py
@@ -37,7 +37,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 30,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 if 'DEV' not in os.environ:
